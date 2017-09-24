9e3e6a85d3571a90b61d3bcd248f5f4facfbde65
OmarNizam <eng.omarnizam@gmail.com>
2017-09-24 13:45:44 +0200

    Preparing & Planning

---
+++ app.js
@@ -0,0 +1,25 @@
+// problem: We need a simple way to look at a user's budge count and a javascript point from a web browser
+
+// solution: Use Node.Js to perform the profile look ups and serve our templete via http
+
+
+// 1- create a web server:
+
+// 2- handle the http route GET / and  POST / i.e Home
+  // if url == "/" && GET
+    // show the searsh field
+  // if url == "/" && POST
+    //redirect to /:username
+
+// 3- handle the HTTP rout GET /:username  i.e /omarnizamaldeen
+  // if url == "/..."
+    //get JSON from treehouse
+      //on "end"
+        // show profile
+      // on "error"
+        // show error
+
+
+// 4- function that handles the reading of files and merge in value
+  // read from file and get a string
+    // merge values in to string 
